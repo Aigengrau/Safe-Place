3ccd4f31af915e8eaed7350168ad5dfe79753f5b
Aigengrau <ochilovanis@gmail.com>
2023-03-09 18:22:52 +0300

    multipages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Quote from "./components/Quote";
+import About from "./components/About";
 import ContactUs from "./components/ContactUs"
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <Router>
+        <Header />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={ContactUs} />
+        </Switch>
       <Quote />
-      <ContactUs/>
-    </div>
+    </Router>
   );
 }
 
